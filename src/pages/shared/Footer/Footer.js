30e97dd85b7214543087c4eb8a7e5926f4b6c244
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-26 19:53:20 +0600

    footed added

--- src/pages/shared/Footer/Footer.js
+++ src/pages/shared/Footer/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/header.png';
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is Footer</h2>
+            <footer className="py-2 bg-dark text-light text-center">
+                <Link
+                    to="/home"
+                    className="d-flex gap-2 justify-content-center align-items-center"
+                >
+                    <img src={logo} alt="" width="60px" />
+                    <h2>Resale books</h2>
+                </Link>
+                <hr />
+                <p>Jabin Tasmin Anny ||<small> Copy right coming soon !!!</small></p>
+            </footer>
         </div>
     );
 };
