8aa449992065320b3061e6705e55044feb376f11
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-27 19:00:38 +0600

    added category section

---
+++ src/pages/Home/AdvertiseItems/AdvertiseItems.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Button, Card } from "react-bootstrap";
+const AdvertiseItems = () => {
+    return (
+        <section className="py-5">
+            <h1 className="d-flex justify-content-center fw-bold text-primary">Products from Our Seller</h1>
+            <div className="item-container container my-5">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                    <div className="col">
+                        <Card>
+                            <Card.Img
+                                variant="top"
+                                src="https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/3:2/w_1998,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg"
+                            />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up
+                                    the bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <Card.Img
+                                variant="top"
+                                src="https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/3:2/w_1998,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg"
+                            />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up
+                                    the bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <Card.Img
+                                variant="top"
+                                src="https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/3:2/w_1998,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg"
+                            />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up
+                                    the bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AdvertiseItems;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import AdvertiseItems from './AdvertiseItems/AdvertiseItems';
 import Banner from './Banner/Banner';
+import Categories from './Categories/Categories';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AdvertiseItems></AdvertiseItems>
+            <Categories></Categories>
         </div>
     );
 };
