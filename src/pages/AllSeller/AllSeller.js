94a77217d707f3be517b8bdd342e44ea943c5124
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-28 18:33:12 +0600

    seller displayed

--- src/pages/AllSeller/AllSeller.js
+++ src/pages/AllSeller/AllSeller.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Sellercard from './Sellercard';
 
 const AllSeller = () => {
+    const sellers = useLoaderData();
+    console.log(sellers);
     return (
-        <div>
+        <div className="">
+            <div className="title">
+                <h1 className="d-flex justify-content-center fw-bold text-primary mt-5 ">All Sellers</h1>
+                <p className="text-center text-primary ">
+                    <span className="text-warning">{sellers.length}</span> Active Sellers
+                </p>
+            </div>
+            <div className="container">
+                {sellers.map((seller) => (
+                    <Sellercard key={seller.uid} seller={seller}></Sellercard>
+                ))}
 
+            </div>
         </div>
     );
 };
